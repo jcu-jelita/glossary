c4d472d91f2cef1ee84e82b4d732a005c40d366c
Pavel Máca <maca.pavel@gmail.com>
2017-07-04 11:18:36 +0200

    add global object CardList and interface ListService

---
+++ src/main/java/global/CardList.java
@@ -0,0 +1,23 @@
+package global;
+
+/**
+ * Created by Pavel Máca <maca.pavel@gmail.com> on 04.07.2017.
+ */
+public class CardList {
+
+    private int id;
+    private String name;
+
+    public CardList(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/global/ListService.java
@@ -0,0 +1,15 @@
+package global;
+
+/**
+ * Created by Pavel Máca <maca.pavel@gmail.com> on 04.07.2017.
+ */
+public interface ListService {
+
+    CardList[] getLitOverview();
+
+    CardList getLitDetail(int id);
+
+    CardList save(CardList cardList);
+
+    boolean remove(int listId);
+}
