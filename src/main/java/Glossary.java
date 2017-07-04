33bab8cda9a077015208f676a5e462a3058f5065
Pavel Máca <maca.pavel@gmail.com>
2017-07-04 09:22:23 +0200

    main class and dummy test

---
+++ src/main/java/Glossary.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Pavel Máca <maca.pavel@gmail.com> on 04.07.2017.
+ */
+public class Glossary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+    }
+}
---
+++ src/test/java/GlossaryTest.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by Pavel Máca <maca.pavel@gmail.com> on 04.07.2017.
+ */
+public class GlossaryTest extends TestCase {
+    public void testMain() throws Exception {
+        assertTrue(true); // dummy test
+    }
+
+}
