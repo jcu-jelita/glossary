19d206daabb85231e8b86e664b9482e80d7d366e
Pavel Máca <maca.pavel@gmail.com>
2017-07-04 14:53:55 +0200

    add words map in CardList

--- src/main/java/glossary/global/CardList.java
+++ src/main/java/glossary/global/CardList.java
@@ -1,5 +1,7 @@
 package glossary.global;
 
+import java.util.HashMap;
+
 /**
  * Created by Pavel Máca <maca.pavel@gmail.com> on 04.07.2017.
  */
@@ -7,10 +9,17 @@ public class CardList {
 
     private int id;
     private String name;
+    private HashMap<Integer, String[]> wordMap;
 
     public CardList(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public CardList(int id, String name, HashMap<Integer, String[]> wordMap) {
+        this.id = id;
+        this.name = name;
+        this.wordMap = wordMap;
     }
 
     public int getId() {
@@ -19,5 +28,9 @@ public class CardList {
 
     public String getName() {
         return name;
+    }
+
+    public HashMap<Integer, String[]> getWordMap() {
+        return wordMap;
     }
 }
