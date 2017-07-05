0ddb5f89c9913748c7bca9adf09adfe22ee5f4e7
Lukáš Vrba <lukasvrbaa@seznam.cz>
2017-07-05 22:06:11 +0300

    added tests for CardListDaoImplTest

--- src/test/java/glossary/engine/dao/CardListDaoImplTest.java
+++ src/test/java/glossary/engine/dao/CardListDaoImplTest.java
@@ -20,7 +20,8 @@ public class CardListDaoImplTest extends DatabaseTestCase {
 
         // add new list
         cardListDao.save(new CardList(0, "Test list"), cardDaoMock.findAllByCardListId(1));
-        // TODO call findAll() = expected size = 1
+        // TODO call findAll() = expected size = 1 -- CHECK&DELETE ME
+        assertEquals(1,cardListDao.findAll().size());
 
     }
 
@@ -32,24 +33,34 @@ public class CardListDaoImplTest extends DatabaseTestCase {
         // save new list with mock Cards
         CardList cardList = new CardList("Test list");
         assertNotNull(cardListDao.save(cardList, cards));
-        // TODO assert carList ID is not NULL
 
-        // TODO save new list with "" as second param = expected NULL
-
-        // TODO save new list with NULL on third param = expected NULL
-
+        // TODO assert carList ID is not NULL -- CHECK&DELETE ME
+        assertNotNull(cardList.getId());
+        // TODO save new list with "" as second param = expected NULL -- CHECK&DELETE ME
+        CardList cardList1 = new CardList("");
+        assertNull(cardListDao.save(cardList1, cards)); //Wrong?
+        // TODO save new list with NULL on third param = expected NULL -- CHECK&DELETE ME
+        // TO CHECK -- Where is the third param?
 
         cardList.setName("New list name");
-        int carListIdOrig = cardList.getId();
-        //TODO save and test if cardList id is same
-
+        int cardListIdOrig = cardList.getId();
+        //TODO save and test if cardList id is same -- CHECK&DELETE ME
+        CardList cl = cardListDao.save(cardList, cards);
+        assertEquals(cardListIdOrig, cl.getId());
 
     }
 
     public void testRemove() throws Exception {
-        // TODO create new list and remove it = expected true
+        // TODO create new list and remove it = expected true -- CHECK&DELETE ME
+        CardDao cardDaoMock = new CardDaoMock();
+        List<Card> cards = cardDaoMock.findAllByCardListId(1);
+        CardListDao cardListDao = new CardListDaoImpl(testConnection, cardDaoMock);
 
-        //TODO try remove non existing list by ID = expected false
+        CardList cl = new CardList(5,"House of Cards");
+        cardListDao.save(cl,cards);
+        assertTrue(cardListDao.remove(5)); // Wrong?
+        //TODO try remove non existing list by ID = expected false -- CHECK&DELETE ME
+        assertFalse(cardListDao.remove(100));
     }
 
 
