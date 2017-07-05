05651592e54c44a749d8eb0025da6cede5b263fe
Lendule <lenka.zavitkovska@gmail.com>
2017-07-05 08:35:46 +0200

    Add photo_DiagramOfProject

---
+++ src/test/java/glossary/engine/service/TestDAOTest.java
@@ -0,0 +1,26 @@
+package glossary.engine.service;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by LeM on 05.07.2017.
+ */
+public class TestDAOTest extends TestCase {
+
+    public void testGetWordCount() throws Exception {
+
+    }
+
+    public void testGetRandomWords() throws Exception {
+
+    }
+
+    public void testCreateWritingTest() throws Exception {
+
+    }
+
+    public void testCretePracticeTest() throws Exception {
+
+    }
+
+}
