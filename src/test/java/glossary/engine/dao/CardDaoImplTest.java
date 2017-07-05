2dd1f2f90cc8a16d5e634ae605779e776e0efc16
Lukáš Vrba <lukasvrbaa@seznam.cz>
2017-07-05 20:06:00 +0300

    added tests for CardDaoImplTest

--- src/test/java/glossary/engine/dao/CardDaoImplTest.java
+++ src/test/java/glossary/engine/dao/CardDaoImplTest.java
@@ -33,15 +33,82 @@ public class CardDaoImplTest extends DatabaseTestCase {
     }
 
     public void testSave() throws Exception {
+        CardDaoImpl cardDao = new CardDaoImpl(testConnection);
+
+        // save new Card without CardList.id
+        Card card = new Card("ice", "led");
+        assertNull(cardDao.save(card));
+
+        // new Card with CardList.id
+        card.setCardListId(1);
+        assertNotNull(cardDao.save(card));
+
+        // update exiting card;
+        card.setWord2("ledovec");
+        int origianlId = card.getId();
+        cardDao.save(card);
+        assertEquals(origianlId, card.getId());
+
+        // empty word1
+        Card emptyWord1 = new Card("", "led");
+        emptyWord1.setCardListId(1);
+        assertNull(cardDao.save(emptyWord1));
+
+        // empty word2
+        Card emptyWord2 = new Card("Ice", "");
+        emptyWord2.setCardListId(1);
+        assertNull(cardDao.save(emptyWord2));
     }
 
     public void testRemove() throws Exception {
+
+
     }
 
     public void testCountByCardListId() throws Exception {
+        CardDaoImpl cardDao = new CardDaoImpl(testConnection);
+
+        assertEquals(0, cardDao.countByCardListId(1));
+
+        Card card = new Card("ice", "led");
+        card.setCardListId(1);
+        cardDao.save(card);
+
+        assertEquals(1, cardDao.countByCardListId(1));
+        assertEquals(0, cardDao.countByCardListId(2));
+
+        Card card2 = new Card("ice", "led");
+        card2.setCardListId(1);
+        cardDao.save(card2);
+        assertEquals(2, cardDao.countByCardListId(1));
+        assertEquals(0, cardDao.countByCardListId(2));
+
     }
 
     public void testPickForTest() throws Exception {
+        CardDaoImpl cardDao = new CardDaoImpl(testConnection);
+
+        // pick from empty list
+        assertEquals(0, cardDao.pickForTest(10, 1).size());
+
+
+        Card card = new Card("ice", "led");
+        card.setCardListId(1);
+        cardDao.save(card);
+        Card card2 = new Card("snow", "snih");
+        card2.setCardListId(1);
+        cardDao.save(card2);
+        Card card3 = new Card("sun", "slunce");
+        card3.setCardListId(1);
+        cardDao.save(card3);
+        //pick one card from list (ID 1) for testing
+        assertEquals(1, cardDao.pickForTest(1, 1).size());
+        //pick two cards from list (ID 1) for testing
+        assertEquals(2, cardDao.pickForTest(2, 1).size());
+        //pick three cards from list (ID 1) for testing
+        assertEquals(3, cardDao.pickForTest(3, 1).size());
+        //pick four cards from list (ID1) for testing - expected 3 only
+        assertEquals(3, cardDao.pickForTest(4, 1).size());
     }
 
 }
