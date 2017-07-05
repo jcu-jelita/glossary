0999705ce65d685d66b094f48b4282d7874b9b43
Pavel Máca <maca.pavel@gmail.com>
2017-07-05 09:12:46 +0200

    BC break: getLitOverview returns ArrayList

--- src/main/java/glossary/global/ListService.java
+++ src/main/java/glossary/global/ListService.java
@@ -1,11 +1,18 @@
 package glossary.global;
 
+import java.util.ArrayList;
+
 /**
  * Created by Pavel Máca <maca.pavel@gmail.com> on 04.07.2017.
  */
 public interface ListService {
 
-    CardList[] getLitOverview();
+    /**
+     * Get array of list objects without words.
+     *
+     * @return When storage is empty, return empty array
+     */
+    ArrayList<CardList> getLitOverview();
 
     CardList getLitDetail(int id);
 
