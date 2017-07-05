df356bd47de83967fc1e8ee618cc086e0ae7da0d
Lukáš Vrba <lukasvrbaa@seznam.cz>
2017-07-05 13:04:40 +0300

    CardDaoMock implementation

--- src/main/java/glossary/engine/dao/CardDaoMock.java
+++ src/main/java/glossary/engine/dao/CardDaoMock.java
@@ -1,26 +1,41 @@
 package glossary.engine.dao;
 
 import glossary.engine.model.Card;
+import glossary.engine.model.CardList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Pavel Máca <maca.pavel@gmail.com> on 05.07.2017.
  */
 public class CardDaoMock implements CardDao {
+    /**
+     *  Get all Cards by List ID
+     * @param listId ID of List
+     * @return a Card List
+     */
     @Override
     public List<Card> findAllByCardListId(int listId) {
-        return null;
+        CardList cl = new CardList();
+        List<Card> al = new ArrayList<>();
+        al.get(cl.getId());
+        return al;
     }
 
     @Override
     public boolean save(Card card) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean remove(int cardId) {
-        return false;
+        return true;
+    }
+
+    @Override
+    public int countByCardListId(int listId) {
+        return 5;
     }
 
     @Override
