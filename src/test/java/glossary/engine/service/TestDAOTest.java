c349e800065a109a3452d2b25b08ac8a20607c24
Lendule <lenka.zavitkovska@gmail.com>
2017-07-05 08:49:15 +0200

    Add photo_DiagramOfProject

--- src/test/java/glossary/engine/service/TestDAOTest.java
+++ src/test/java/glossary/engine/service/TestDAOTest.java
@@ -8,19 +8,32 @@ import junit.framework.TestCase;
 public class TestDAOTest extends TestCase {
 
     public void testGetWordCount() throws Exception {
+        TestDAO testDAO = new TestDAO();
 
+        assertEquals(0, testDAO.getWordCount(-1));
+
+        // TODO vytvořit seznam a ID uložit do idList
+        int idList = 1;
+        assertEquals(10, testDAO.getWordCount(idList));
     }
 
     public void testGetRandomWords() throws Exception {
+        TestDAO ... = new TestDAO();
 
+        assertEquals(testGetRandomWords(););
     }
 
     public void testCreateWritingTest() throws Exception {
+        TestDAO ... = new TestDAO();
+
+        assertEquals(testCreateWritingTest(););
 
     }
 
     public void testCretePracticeTest() throws Exception {
+        TestDAO ... = new TestDAO();
 
+        assertEquals(testCretePracticeTest(););
     }
 
 }
