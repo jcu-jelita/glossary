a4ce0afdec3abcb89a5a1a0710ed8f8678b56710
Lukáš Vrba <lukasvrbaa@seznam.cz>
2017-07-05 15:08:29 +0300

    Updated CardDaoMock implementation - commented & Method PickFortest

--- src/main/java/glossary/engine/dao/CardDaoMock.java
+++ src/main/java/glossary/engine/dao/CardDaoMock.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public class CardDaoMock implements CardDao {
     /**
+     * TESTING MOCK OBJECT
      * Mock object of getting all Cards by List ID
      *
      * @param listId ID of List
@@ -33,7 +34,8 @@ public class CardDaoMock implements CardDao {
     }
 
     /**
-     *  Method save for saving a Card
+     * TESTING MOCK OBJECT
+     * Method save for saving a Card
      * @param card Card for saving
      * @return true always for testing purposes
      */
@@ -43,6 +45,7 @@ public class CardDaoMock implements CardDao {
     }
 
     /**
+     * TESTING MOCK OBJECT
      * Removing Card by its ID
      * @param cardId ID of card we want to remove
      * @return true always for testing purposes
@@ -53,12 +56,35 @@ public class CardDaoMock implements CardDao {
     }
 
     /**
-     *  Method returning number of Cards included in one List
+     * TESTING MOCK OBJECT
+     * Method returning number of Cards included in one List
      * @param listId ID of List we want to know number of cards
      * @return a integer of number of cards
      */
     @Override
     public int countByCardListId(int listId) {
         return 2;
+    }
+
+    /**
+     * TESTING MOCK OBJECT
+     * Method for picking up specific test
+     * @param wordCount number of words in test
+     * @param cardListId Selected List for testing
+     * @return specific List of Cards we want to test/practise
+     */
+    @Override
+    public List<Card> pickForTest(int wordCount, int cardListId) {
+        List<Card> al = new ArrayList<>();
+        Card card1 = new Card(1, 1, "Apple", "Jablko");
+        Card card2 = new Card(2, 1, "Pear", "Hruška");
+        Card card3 = new Card(3, 2, "Windows", "Okna");
+        Card card4 = new Card(4, 1, "Pineapple", "Ananas");
+        al.add(card1);
+        al.add(card2);
+        al.add(card3);
+        al.add(card4);
+        // TODO Pája review / What should be done in this method
+        return al;
     }
 }
